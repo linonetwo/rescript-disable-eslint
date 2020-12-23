1de541dcdfe8fc80ef0720fc337452617bbc0a42
Eugene Datsky <eugene@datsky.ru>
2020-12-23 16:00:01 +1100

    Support react-scripts 4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-export default config => {
-  // find the eslint rule config
-  const index = config.module.rules.findIndex(
-    ruleObj =>
-      Array.isArray(ruleObj.use) && ruleObj.use.length > 0 && typeof ruleObj.use[0].options.useEslintrc === 'boolean'
-  );
-  // remove it
-  config.module.rules.splice(index, 1);
-  return config;
-};
+import {removeWebpackPlugin} from '@rescripts/utilities';
+
+export default config => removeWebpackPlugin('ESLintWebpackPlugin', config)
+
